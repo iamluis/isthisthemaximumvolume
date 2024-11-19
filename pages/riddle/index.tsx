de64c447084f9f7fb615432f0ee3ce64ad98b6fa
iamluis <47565792+iamluis@users.noreply.github.com>
2024-11-19 18:12:12 +0100

    feat: import use statef

--- pages/riddle/index.tsx
+++ pages/riddle/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState } from 'react'
 import styles from '../../styles/Riddle.module.css'
 
 const Home: NextPage = () => {
