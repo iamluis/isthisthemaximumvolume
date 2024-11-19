1c75b31a5dc2c57af952b4e197c81329d3da7c1c
iamluis <47565792+iamluis@users.noreply.github.com>
2024-11-19 18:16:44 +0100

    feat: errors

--- pages/riddle/index.tsx
+++ pages/riddle/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import { useState } from 'react'
-import styles from '../styles/Riddle.module.css'
+import styles from '../../styles/Riddle.module.css'
 
 const Home: NextPage = () => {
   const [answer, setAnswer] = useState('')
